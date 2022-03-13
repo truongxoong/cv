560e473a6f8bbd951725a439d1742f9618af27ae
Cu Truong <truongxoong@gmail.com>
2022-03-13 17:57:43 +0700

    hihi

--- cv.js
+++ cv.js
@@ -4,36 +4,82 @@ let slide = document.querySelectorAll('.slide')
 let Up = document.querySelector('.fa-angle-up')
 let Down = document.querySelector('.fa-angle-down')
 let progress = document.querySelectorAll('.progress-bar')
+let English = document.querySelector('.englishSkills > .english_read')
+let Personal = document.querySelector('.personalSkills > .english_read')
+let h3 = document.querySelectorAll('.all h3')
+let iResume = document.querySelectorAll('.education h2 i')
+let h3Resume = document.querySelectorAll('.eduContent .borderBottom')
+let Doc = document.querySelector('.education .doc')
 
 function update(x) {
     slide.forEach(function(link) {
         link.classList.remove('up')
         link.classList.remove('down')
         link.classList.remove('iconMenu')
-        // progress.forEach(function(ele) {
-        //     ele.classList.remove('addAbout')
-        // })
     })
-    let y = slide[4]
-    console.log(y.style.width)
-    // if(y == 1) {
-    //     Contact.style.pointerEvents = 'none'
-    // }else {
-    //     Contact.style.pointerEvents = 'auto'
-    // }
     a = x
-    progress.forEach(function(ele) {
-        ele.classList.add('addAbout')
-    })
     slide[x].classList.add('iconMenu')
+}
+
+function ProSkills() {
+    progress.forEach(function(ele) {
+        ele.classList.remove('addAbout')
+    })
+    setInterval(function() {
+        progress.forEach(function(ele) {
+            ele.classList.add('addAbout')
+        })
+    }, 10) 
+
+    English.classList.remove('english')
+    setInterval(function() {
+        English.classList.add('english')
+    }, 10) 
+
+    Personal.classList.remove('personal')
+    setInterval(function() {
+        Personal.classList.add('personal')
+    }, 10) 
+
+    h3.forEach(function(ele) {
+        ele.classList.remove('skillsH3')
+    })
+    setInterval(function() {
+        h3.forEach(function(ele) {
+            ele.classList.add('skillsH3')
+        })
+    }, 10)
+    
+    iResume.forEach(function(ele) {
+        ele.classList.remove('iconResume')
+    })
+    setInterval(function() {
+        iResume.forEach(function(ele) {
+            ele.classList.add('iconResume')
+        })
+    }, 10)
+    
+    h3Resume.forEach(function(ele) {
+        ele.classList.remove('textEdu')
+    })
+    setInterval(function() {
+        h3Resume.forEach(function(ele) {
+            ele.classList.add('textEdu')
+        })
+    }, 10)
+
+    Doc.classList.remove('Doc')
+    setInterval(function() {
+        Doc.classList.add('Doc')
+    }, 10)   
 }
 
 iconLeft.forEach(function(btn, index) {
     btn.addEventListener('click', function() {
         update(index)
+        ProSkills()
     })
 })
-
 // -----------------------------
 
 Down.addEventListener('click', function() {
@@ -42,6 +88,7 @@ Down.addEventListener('click', function() {
     slide[a].classList.remove('iconMenu')
     a = (a+1) % slide.length
     slide[a].classList.add('down')
+    ProSkills()
 })
 Up.addEventListener('click', function() {
     slide[a].classList.remove('up')
@@ -49,6 +96,7 @@ Up.addEventListener('click', function() {
     slide[a].classList.remove('iconMenu')  
     a = ((a-1) + slide.length) % slide.length
     slide[a].classList.add('up')
+    ProSkills()
 })
 
 // Up.addEventListener('click', function() {
